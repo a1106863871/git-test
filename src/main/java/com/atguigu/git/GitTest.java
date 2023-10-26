a4729bf9cc24b05f041cf06116105952dbd885bc
xxj <1106863871@qq.com>
2023-10-27 04:44:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ * ClassName:GitTest
+ * Package:com.atguigu.git
+ * Description:
+ *
+ * @Author 大大怪将军
+ * @Create 2023/10/27 4:37
+ * @Version 1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
